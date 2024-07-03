24bb265cc14b0b47f82cb9b223d70581e950ed74
Juan Ledesma <umbranito@gmail.com>
2024-07-03 18:25:47 -0400

    Remove post

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => (
                             <div className="content-box">
                                 <h2><span className="first-word">¡Separa tus</span> <span className="last-word">Boletos!</span></h2>
 
-                                <Form name="Contacto" method="POST" data-netlify="true" netlify>
+                                <Form name="Contacto" data-netlify="true" netlify>
                                     <input type="hidden" name="form-name" value="contact"></input>
                                     <Form.Group className="mb-3" controlId="formFirstName">
                                         <Form.Label><h3>Nombre</h3></Form.Label>
